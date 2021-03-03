7495bf1921b64de2a518d7571cc04258351c520d
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2021-03-03 09:03:29 +0900

    2021/03/03
    2021/03/03

--- 백준/백준.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-var str = "백준 알고리즘입니다."
-
-// https://www.acmicpc.net
--- 백준/백준.playground/Sources/AMinusB.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-func minus() {
-    let nums = readLine()!.split(separator: " ").map {Int($0)!}
-
-    print(nums[0] - nums[1])
-}
